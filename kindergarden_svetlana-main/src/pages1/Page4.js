7c347e116991d8700382acebe5daf3c83fda9b30
ABDURAHIMhalilov <133384666+ABDURAHIMhalilov@users.noreply.github.com>
2023-06-07 20:05:56 +0500

    s
    s

--- kindergarden_svetlana-main/src/pages1/Page4.js
+++ kindergarden_svetlana-main/src/pages1/Page4.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function Page4() {
+    useEffect(() => {
+        axios
+    }, [])
   return (
     <div>
-      
+      <div className='Test-Big'>
+        <div className='Test-Img'>
+            <img src='' alt='' />
+        </div>
+        <div className='Test-Variant'>
+          <p></p>
+        </div>
+      </div>
     </div>
   )
 }
