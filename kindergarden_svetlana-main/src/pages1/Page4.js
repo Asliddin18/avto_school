70ba4401709bf7625ded997453d9a004bab3085e
ABDURAHIMhalilov <133384666+ABDURAHIMhalilov@users.noreply.github.com>
2023-06-08 09:41:55 +0500

    qq
    qq

--- kindergarden_svetlana-main/src/pages1/Page4.js
+++ kindergarden_svetlana-main/src/pages1/Page4.js
@@ -1,14 +1,19 @@
 import React, { useEffect } from 'react'
+import url from '../host'
+import axios from 'axios'
+
 
 export default function Page4() {
-    useEffect(() => {
-        axios
-    }, [])
+  useEffect(() => {
+    axios.get(`${url}/test`).then(res => {
+      console.log(res.data);
+    })
+  }, [])
   return (
     <div>
       <div className='Test-Big'>
         <div className='Test-Img'>
-            <img src='' alt='' />
+          <img src='' alt='' />
         </div>
         <div className='Test-Variant'>
           <p></p>
